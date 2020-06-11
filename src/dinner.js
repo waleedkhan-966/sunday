ad66524df52cc9708263959445cace0af4d7e784
Waleed Iqbal <56128827+waleedkhan-966@users.noreply.github.com>
2020-06-11 21:08:19 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Dinner(props){
     return(
         <div>
-            <h1>we are making child component {props.lineName}</h1>
+            <h1>we are making child component for all {props.lineName}</h1>
             <hi>we are again making child component {props.secondName}</hi>
             
         </div>
